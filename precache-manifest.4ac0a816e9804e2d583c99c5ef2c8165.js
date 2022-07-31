c07e73fea776681b4b5c5ccea662d0e2a315f65d
bbasanta238 <bbasanta238@gmail.com>
2022-07-31 16:38:06 +0545

    Updates

---
+++ precache-manifest.4ac0a816e9804e2d583c99c5ef2c8165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d5090b7bdd0b1bcb579",
+    "url": "/todo-list/static/css/main.127fa297.chunk.css"
+  },
+  {
+    "revision": "1d5090b7bdd0b1bcb579",
+    "url": "/todo-list/static/js/main.1d5090b7.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/todo-list/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/todo-list/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "00cc1de418b1725d6f30",
+    "url": "/todo-list/static/js/runtime~main.00cc1de4.js"
+  },
+  {
+    "revision": "a91eb71fc6adff78a29dc5718f579fa0",
+    "url": "/todo-list/index.html"
+  }
+];
